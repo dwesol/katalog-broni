0ee7fe90aa2c1f74363638a8999dd0e4ddeae654
dwesol <38853883+dwesol@users.noreply.github.com>
2026-03-17 22:12:32 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,127 @@
 const katalog = {
 
 pistolety: [
-{ producent:"Glock", kaliber:"9x19", model:"Glock 17", img:"img/glock17.jpg" },
-{ producent:"Glock", kaliber:"9x19", model:"Glock 19", img:"img/glock19.jpg" },
-{ producent:"Walther", kaliber:"9x19", model:"PDP", img:"img/pdp.jpg" },
-{ producent:"Walther", kaliber:"22lr", model:"PDP22", img:"img/pdp.jpg" },
-{ producent:"SIG Sauer", kaliber:".357 SIG", model:"P229", img:"img/pdp.jpg" }
+
+{
+producent:"Glock",
+kaliber:"9x19",
+model:"Glock 17",
+img:"img/glock17.jpg",
+
+opis:"Glock 17 to jeden z najbardziej rozpoznawalnych pistoletów na świecie. Charakteryzuje się prostą konstrukcją, wysoką niezawodnością i dużą pojemnością magazynka. Jest szeroko używany przez służby mundurowe oraz strzelców sportowych.",
+
+spec:{
+"Kaliber":"9x19",
+"Długość lufy":"11,4 cm",
+"Pojemność magazynka":"17",
+"Całkowita długość":"20,2 cm"
+}
+},
+
+{
+producent:"Glock",
+kaliber:"9x19",
+model:"Glock 19",
+img:"img/glock19.jpg",
+
+opis:"Kompaktowa wersja Glocka 17, idealna do skrytego noszenia. Łączy niewielkie rozmiary z wysoką pojemnością magazynka i niezawodnością typową dla konstrukcji Glock.",
+
+spec:{
+"Kaliber":"9x19",
+"Długość lufy":"10,2 cm",
+"Pojemność magazynka":"15",
+"Całkowita długość":"18,7 cm"
+}
+},
+
+{
+producent:"Walther",
+kaliber:"9x19",
+model:"PDP",
+img:"img/pdp.jpg",
+
+opis:"Nowoczesny pistolet Walther PDP zaprojektowany z myślą o ergonomii i szybkim strzelaniu. Charakteryzuje się doskonałym chwytem oraz możliwością montażu kolimatora.",
+
+spec:{
+"Kaliber":"9x19",
+"Długość lufy":"10,2 cm",
+"Pojemność magazynka":"15",
+"Całkowita długość":"18,4 cm"
+}
+},
+
+{
+producent:"Walther",
+kaliber:".22 LR",
+model:"PDP22",
+img:"img/pdp.jpg",
+
+opis:"Lekka wersja treningowa popularnego modelu PDP w kalibrze .22 LR. Idealna do nauki strzelania i treningu przy niższych kosztach amunicji.",
+
+spec:{
+"Kaliber":".22 LR",
+"Długość lufy":"10 cm",
+"Pojemność magazynka":"12",
+"Całkowita długość":"18 cm"
+}
+},
+
+{
+producent:"SIG Sauer",
+kaliber:".357 SIG",
+model:"P229",
+img:"img/pdp.jpg",
+
+opis:"SIG Sauer P229 to solidny pistolet służbowy, znany z wytrzymałości i precyzji. W wersji .357 SIG oferuje wysoką energię pocisku i bardzo dobrą balistykę.",
+
+spec:{
+"Kaliber":".357 SIG",
+"Długość lufy":"9,9 cm",
+"Pojemność magazynka":"12",
+"Całkowita długość":"18 cm"
+}
+}
+
 ],
 
 pm: [
-{ producent:"HK", kaliber:"9x19", model:"MP5", img:"img/mp5.jpg" }
+
+{
+producent:"HK",
+kaliber:"9x19",
+model:"MP5",
+img:"img/mp5.jpg",
+
+opis:"Legendarny pistolet maszynowy MP5 produkowany przez Heckler & Koch. Znany z doskonałej celności i kontroli podczas ognia ciągłego.",
+
+spec:{
+"Kaliber":"9x19",
+"Długość lufy":"22,5 cm",
+"Pojemność magazynka":"30",
+"Całkowita długość":"68 cm"
+}
+}
+
 ],
 
 karabiny: [
-{ producent:"AK", kaliber:"7.62x39", model:"AK-47", img:"img/ak47.jpg" }
+
+{
+producent:"AK",
+kaliber:"7.62x39",
+model:"AK-47",
+img:"img/ak47.jpg",
+
+opis:"AK-47 to jeden z najbardziej znanych karabinów automatycznych na świecie. Słynie z prostoty, niezawodności i odporności na trudne warunki.",
+
+spec:{
+"Kaliber":"7.62x39",
+"Długość lufy":"41,5 cm",
+"Pojemność magazynka":"30",
+"Całkowita długość":"87 cm"
+}
+}
+
 ]
 
 }
