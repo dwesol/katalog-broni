6826899be5311cfedbe87ac4f4447888ffa3ae43
dwesol <38853883+dwesol@users.noreply.github.com>
2026-03-17 20:36:30 +0100

    Update data.js

--- data.js
+++ data.js
@@ -3,8 +3,9 @@ const katalog = {
 pistolety: [
 { producent:"Glock", kaliber:"9x19", model:"Glock 17", img:"img/glock17.jpg" },
 { producent:"Glock", kaliber:"9x19", model:"Glock 19", img:"img/glock19.jpg" },
-{ producent:"Walther", kaliber:"9x19", model:"PDP", img:"img/pdp.jpg" }
-{ producent:"Walther", kaliber:"22lr", model:"PDP22", img:"img/pdp22.jpg" }
+{ producent:"Walther", kaliber:"9x19", model:"PDP", img:"img/pdp.jpg" },
+{ producent:"Walther", kaliber:"22lr", model:"PDP22", img:"img/pdp22.jpg" },
+{ producent:"SIG Sauer", kaliber:".357 SIG", model:"P229", img:"img/p229.jpg" }
 ],
 
 pm: [
