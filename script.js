7a187d944c1108f1af6c5dd0cca6eee27c5808bf
dwesol <38853883+dwesol@users.noreply.github.com>
2026-03-19 22:45:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,55 +23,51 @@ let tableHTML = ""
 function renderCategory(cat){
 
 currentData = katalog[cat]
+
+// kalibry (globalne)
 kalibry = [...new Set(currentData.map(x=>x.kaliber))]
 
+// sortowanie kalibrów
 kalibry.sort((a,b)=>{
 return kolejnoscKalibrow.indexOf(a) - kolejnoscKalibrow.indexOf(b)
 })
-  
+
+// producenci
+let producenci = [...new Set(currentData.map(x=>x.producent))]
+
+// sortowanie alfabetyczne
+producenci.sort((a,b)=>a.localeCompare(b))
+
+// podział
+const glowni = producenci.filter(p=>glowniProducenci.includes(p))
+const pozostali = producenci.filter(p=>!glowniProducenci.includes(p))
+
+// start tabeli
 tableHTML = "<table>"
-  
-const data = katalog[cat]
 
-const producenci = [...new Set(data.map(x=>x.producent))]
-const kalibry = [...new Set(data.map(x=>x.kaliber))]
-
-let html = "<table>"
-
-html += "<tr><th>Producent</th>"
+// nagłówek
+tableHTML += "<tr><th>Producent</th>"
 
 kalibry.forEach(k=>{
-html += "<th>"+k+"</th>"
+tableHTML += "<th>"+k+"</th>"
 })
 
-html += "</tr>"
+tableHTML += "</tr>"
 
-producenci.forEach(p=>{
-
-html += "<tr>"
-html += "<td>"+p+"</td>"
-
-kalibry.forEach(k=>{
-
-const modele = data.filter(x=>x.producent==p && x.kaliber==k)
-
-html += "<td><ul>"
-
-modele.forEach(m=>{
-html += `<li class="model" data-id="${m.model}" data-img="${m.img}">${m.model}</li>`
+// główni producenci
+glowni.forEach(p=>{
+generateRow(p)
 })
 
-html += "</ul></td>"
+// pozostali
+if (pozostali.length > 0){
+generateRow("Pozostali", pozostali)
+}
 
-})
+tableHTML += "</table>"
 
-html += "</tr>"
-
-})
-
-html += "</table>"
-
-document.getElementById("table-container").innerHTML = html
+// wstawienie do HTML
+document.getElementById("table-container").innerHTML = tableHTML
 
 attachHover()
 
