2736fe0f0e98475d1dd3161c2e4372dbf0454bbc
dwesol <38853883+dwesol@users.noreply.github.com>
2026-03-28 00:55:09 +0100

    Update data.js

--- data.js
+++ data.js
@@ -38,7 +38,7 @@ spec:{
 producent:"Ceska Zbrojovka",
 kaliber:"22 LR",
 model:"CZ 122",
-img:"cz122.jpg",
+img:"img/cz122.jpg",
 
 opis:`<p>CZ 122 jest bronią samopowtarzalną, działającą na zasadzie odrzutu zamka swobodnego. Mechanizm spustowo-uderzeniowy kurkowy, z samonapinaniem (SA/DA). Pistolet posiada bezpiecznik nastawny, przetykowy. Pistolet zasilany jest z magazynków pudełkowych o pojemności 10 naboi. Po wystrzeleniu ostatniego naboju zamek zatrzymuje się w tylnej pozycji na zatrzasku zamka. Zewnętrzna dźwignia zatrzasku znajduje się po obu stronach szkieletu. Przyrządy celownicze otwarte, składają się z muszki i szczerbinki.</p>`,
 
@@ -54,7 +54,7 @@ spec:{
 producent:"Ceska Zbrojovka",
 kaliber:"9x19",
 model:"CZ P-09",
-img:"czp09.jpg",
+img:"img/czp09.jpg",
 
 opis:`<p>Pistolet CZ P-09 to bardzo udane przeniesienie wszystkich najlepszych cech kultowych pistoletów CZ 75 i CZ 75 SP-01 do polimerowego szkieletu. Czeskiej firmie udało się zachować zarówno świetne działanie pistoletu, znane i cenione od dawna, dodając nowoczesny i lekki szkielet. Uzyskano w ten sposób bardzo interesującą jednostkę, na dodatek tanią. CZ P-09 i P-07 (jej mniejszy, kompaktowy brat) to przez wiele lat przeboje sprzedażowe Ceskiej Zbrojovki.</p>`,
 
@@ -70,7 +70,7 @@ spec:{
 producent:"Ceska Zbrojovka",
 kaliber:"9x19",
 model:"CZ SHADOW 2",
-img:"czshadow2.jpg",
+img:"img/czshadow2.jpg",
 
 opis:`<p>CZ 75 SP-01 Shadow- Nowa generacja pistoletu CZ 75 SP-01, specjalnie dostosowana według sugestii użytkowników ze społeczności na całym świecie, przy dodatkowym udziale strzelców Team CZ Angusa Hobdella i Adama Tyca. Oparty na SP-01, nie ma blokady iglicy, co poprawia skok spustu. Posiada również nieco zmieniony chwyt i zabezpieczenie, „słabszą” sprężynę odrzutową ułatwiającą ładowanie, a także światłowodową muszkę i taktyczną muszkę</p><br><p>CZ 75 Shadow 2- W 2016 roku, przy współpracy elitarnych strzelców IPSC drużyny Česká zbrojovka, wypuszczono ulepszoną wersję Shadow, zwaną Shadow 2. Zawierała dłuższą lufę, przekształcony, lżejszy zamek, bardziej agresywne ząbki zamka, poprawiona ergonomia chwytu, agresywna kratka chwytu i mniejszy światłowód w muszce.</p>`,
 
@@ -86,7 +86,7 @@ spec:{
 producent:"Colt's Manufacturing Company",
 kaliber:".45 ACP",
 model:"COLT M1911",
-img:"coltm1911.jpg",
+img:"img/coltm1911.jpg",
 
 opis:`<p>Colt M1911 (desygnata wojskowa: Automatic Pistol, Caliber .45, M1911; pot. Colt Government) – amerykański pistolet samopowtarzalny kalibru .45 cala. Wywodzi się z konstrukcji zaprojektowanej przez Johna Browninga w 1900 roku. W 1911 roku Colt M1911 wszedł na wyposażenie armii amerykańskiej, a w 1926 po niewielkich modyfikacjach dokonanych w wyniku doświadczeń I wojny światowej, opracowano wersję M1911A1 (Automatic Pistol, Caliber .45, M1911A1), którą można rozpoznać po podfrezowaniu w okolicy spustu ułatwiającym strzelanie osobom o krótszych palcach, oraz tylnej części chwytu (poniżej bezpiecznika) z frezowanymi podłużnymi nacięciami zmniejszającymi obracanie się broni na boki.</p>`,
 
