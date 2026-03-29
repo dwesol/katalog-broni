412a409e41ea06b740ae6b68f4c912c460344ef9
dwesol <38853883+dwesol@users.noreply.github.com>
2026-03-29 20:23:00 +0200

    Update data.js

--- data.js
+++ data.js
@@ -200,7 +200,7 @@ kaliber:".22 LR",
 model:"MK IV",
 img:"img/rm4.jpg",
 
-	opis:`<p>Ruger MKII to półautomatyczny pistolet bocznego zapłonu występujący w innych wersjach: Standard, MK I Target, MK III i MK IV. Jest sprzedawany jako niedrogi pistolet bocznego zapłonu kalibru .22 przeznaczony do rekreacyjnego sportu i strzelania do celu oraz strzelania rekreacyjnego. Zaprojektowany przez założyciela firmy Williama B. Rugera. model Standard i jego kolejne wersje stały się najbardziej akceptowanymi i udanymi półautomatycznymi pistoletami kalibru .22, jakie kiedykolwiek wyprodukowano.</p>`,
+opis:`<p>Ruger MKII to półautomatyczny pistolet bocznego zapłonu występujący w innych wersjach: Standard, MK I Target, MK III i MK IV. Jest sprzedawany jako niedrogi pistolet bocznego zapłonu kalibru .22 przeznaczony do rekreacyjnego sportu i strzelania do celu oraz strzelania rekreacyjnego. Zaprojektowany przez założyciela firmy Williama B. Rugera. model Standard i jego kolejne wersje stały się najbardziej akceptowanymi i udanymi półautomatycznymi pistoletami kalibru .22, jakie kiedykolwiek wyprodukowano.</p>`,
 
 spec:{
 "Kaliber":".22 LR",
